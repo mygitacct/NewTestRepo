bc895e4c38b5f18f1e6faf20ce4b3983aec95015
jdubb@nmr.mgh.harvard.edu <jdubb@nmr.mgh.harvard.edu>
2015-08-10 19:39:52 -0400

    adding project helloworld to local repo

---
+++ HelloWorldQT/HelloWorldQT.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-04-09T23:58:20
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = HelloWorldQT
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ HelloWorldQT/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+
+int main()
+{
+    int x=4;
+
+    printf("Hello World. This is total bullshit=%d\n", x++);
+    printf("Hello World. This is total bullshit=%d\n", x++);
+    printf("Hello World. This is total bullshit=%d\n", x++);
+    printf("Hello World. This is total bullshit=%d\n", x++);
+    printf("Hello World. This is total bullshit=%d\n", x++);
+    printf("Hello World. This is total bullshit=%d\n", x++);
+
+    return 0;
+}
